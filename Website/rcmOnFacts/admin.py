4f3af0a746defe322ae7c2210482995e218f517b
orzzzl <zhuangzeleng19920731@gmail.com>
2015-07-25 02:47:04 -0400

    remove something

--- Website/rcmOnFacts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from rcmOnFacts.models import cityData
-
-# Register your models here.
-
-admin.site.register(cityData)
--- Website/rcmOnFacts/urls.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'zelengzhuang'
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^questions', views.questions, name='questions'),
-    url(r'^res', views.res, name='res')
-]
